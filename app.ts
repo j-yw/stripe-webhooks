d5ccfe8e8351557679452bf6cddc5a5dddaedb79
j-yw <32629001+j-yw@users.noreply.github.com>
2024-02-11 12:26:32 +0800

    fix: add PORT var for railway

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from "express";
 import axios from "axios";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
