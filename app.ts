b9c5878f78e6140834ea12f801c0b208480a9577
j-yw <32629001+j-yw@users.noreply.github.com>
2024-02-11 12:43:31 +0800

    fix: get customer email

--- app.ts
+++ app.ts
@@ -10,19 +10,18 @@ app.use(express.json());
 app.post("/stripe-webhook", async (req: Request, res: Response) => {
 	try {
 		const eventType = req.body.type;
-		console.log(`🍀 \n | 🍄 app.post \n | 🍄 req:`, req);
+		const customer_email = req.body.data.object.customer_email;
+
+		console.log(
+			`🍀 \n | 🍄 app.post \n | 🍄 customer_email:`,
+			customer_email
+		);
 		console.log(`🍀 \n | 🍄 app.post \n | 🍄 eventType:`, eventType);
 
 		if (eventType !== "checkout.session.completed") {
 			console.log(`Received unsupported event type: ${eventType}`);
 			return res.sendStatus(400);
 		}
-
-		const {
-			data: {
-				object: { customer_email },
-			},
-		} = req.body;
 
 		await axios.post(
 			"https://60c6-38-6-227-3.ngrok-free.app/api/updateUserRole",
