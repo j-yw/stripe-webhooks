c1580bf9345615592125671f1c8433e316488592
j-yw <32629001+j-yw@users.noreply.github.com>
2024-02-16 15:30:10 +0800

    fix:routes

--- app.ts
+++ app.ts
@@ -10,52 +10,6 @@ dotenv.config();
 app.use(express.json());
 
 app.post("/stripe-webhook", async (req: Request, res: Response) => {
-	try {
-		const eventType = req.body.type;
-		const customer_email = req.body.data.object.customer_email;
-
-		console.log(
-			`🍀 \n | 🍄 app.post \n | 🍄 customer_email:`,
-			customer_email
-		);
-		console.log(`🍀 \n | 🍄 app.post \n | 🍄 eventType:`, eventType);
-
-		if (eventType !== "checkout.session.completed") {
-			console.log(`Received unsupported event type: ${eventType}`);
-			return res.sendStatus(400);
-		}
-
-		const formData = new FormData();
-		formData.append("email", customer_email);
-		formData.append("role", "PREMIUM");
-
-		await axios
-			.post(
-				"https://60c6-38-6-227-3.ngrok-free.app/api/updateUserRole",
-				formData,
-				{
-					headers: {
-						"x-api-key": process.env.X_API_KEY,
-					},
-				}
-			)
-			.then((result) => {
-				console.log(result);
-				res.status(200);
-			})
-			.catch((err) => {
-				console.error(err);
-				res.status(500).json({ message: "Internal Server Error" });
-			});
-	} catch (error) {
-		console.error("Error handling webhook event:", error);
-		res.sendStatus(500);
-	}
-});
-
-//
-
-app.post("/stripe-webhook", async (req: Request, res: Response) => {
 	const eventType = req.body.type;
 	const customer_email = req.body.data.object.customer_email;
 	try {
